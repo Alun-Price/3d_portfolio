717551eb1c1ed4c3a898e533d5eaf2f5d6482ce2
Alun Price <alunprice089@gmail.com>
2023-03-06 17:12:51 +0000

    added postcss and autoprefixer [required for TailwindCSS]; added active link and mobile to navbar; completed navbar component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logoapsphere from "./logoapsphere.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -32,6 +33,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  logoapsphere,
   backend,
   creator,
   mobile,
